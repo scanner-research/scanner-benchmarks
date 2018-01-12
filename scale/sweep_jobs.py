af87e09ce4105c9a78f42982d3c78ee771e070bd
Alex Poms <alexpoms@gmail.com>
2018-01-11 17:33:55 -0800

    Run jobs quietly

--- scale/sweep_jobs.py
+++ scale/sweep_jobs.py
@@ -5,24 +5,31 @@ workloads = ['hist', 'pose']
 datasets = ['tvnews']
 cpu_nodes = [10, 50, 100, 150, 200, 250, 300]
 
+    subprocess.check_call(['java', '-jar', 'foo.jar'], stdout=devnull)
+
+def run_quiet(cmd):
+    with open(os.devnull, 'w') as devnull:
+        return subprocess.check_call(cmd, shell=True,
+                                     stdout=devnull, stderr=devnull)
 # Shutdown nodes
-subprocess.check_call('cinematography_shutdown_nodes.sh 2 {:d}'.format(500))
+run_quiet('bash cinematography_shutdown_nodes.sh 2 {:d}'.format(500))
 
 cpu_results = []
 for nodes in cpu_nodes:
     # Spawn nodes
-    subprocess.check_call('cinematography_spawn_nodes.sh {:d}'.format(nodes))
+    run_quiet('bash cinematography_spawn_nodes.sh {:d}'.format(nodes))
     # Run job
     for workload in workloads:
         for dataset in datasets:
             print('Running: {:s} {:s} {:d}'.format(dataset, workload, nodes))
-            subprocess.check_call('python scale_job.py {:s} {:s} {:d}'.format(
+            rv = run_quiet('python scale_job.py {:s} {:s} {:d}'.format(
                 dataset,
                 workload,
                 nodes))
+            print('Return value: {:d}'.format(rv))
             # Read total time
             with open('time.txt', 'r') as f:
                 total_time = float(f.read())
             print('Total time: {:.2f}'.format(total_time))
     # Shutdown nodes
-    subprocess.check_call('cinematography_shutdown_nodes.sh 2 {:d}'.format(nodes))
+    run_quiet('bash cinematography_shutdown_nodes.sh 2 {:d}'.format(nodes))
