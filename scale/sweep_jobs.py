98be85e0ab0287e46975ff99f8169195d6c9ffbc
Alex Poms <alexpoms@gmail.com>
2018-01-11 17:36:30 -0800

    Add informative print statements

--- scale/sweep_jobs.py
+++ scale/sweep_jobs.py
@@ -1,21 +1,22 @@
 import subprocess
+import os
 
 workloads = ['hist', 'pose']
 
 datasets = ['tvnews']
 cpu_nodes = [10, 50, 100, 150, 200, 250, 300]
 
-    subprocess.check_call(['java', '-jar', 'foo.jar'], stdout=devnull)
-
 def run_quiet(cmd):
     with open(os.devnull, 'w') as devnull:
         return subprocess.check_call(cmd, shell=True,
                                      stdout=devnull, stderr=devnull)
 # Shutdown nodes
+print('Running initial node shutdown...')
 run_quiet('bash cinematography_shutdown_nodes.sh 2 {:d}'.format(500))
 
 cpu_results = []
 for nodes in cpu_nodes:
+    print('Spawning {:d} nodes...'.format(nodes))
     # Spawn nodes
     run_quiet('bash cinematography_spawn_nodes.sh {:d}'.format(nodes))
     # Run job
@@ -31,5 +32,6 @@ for nodes in cpu_nodes:
             with open('time.txt', 'r') as f:
                 total_time = float(f.read())
             print('Total time: {:.2f}'.format(total_time))
+    print('Shutting down {:d} nodes...'.format(nodes))
     # Shutdown nodes
     run_quiet('bash cinematography_shutdown_nodes.sh 2 {:d}'.format(nodes))
